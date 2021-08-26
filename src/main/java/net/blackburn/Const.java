f74a26f1dada0df08485648f478081b35f7ed0d7
NicholasBlackburn1 <nickblackburn02@gmail.com>
2021-08-26 14:35:03 -0400

    got logo to workie UwU

--- src/main/java/net/blackburn/Const.java
+++ src/main/java/net/blackburn/Const.java
@@ -1,14 +1,14 @@
 
 /***
- * this is werre i
+ * this is were  idefine all consts for my additions to mc 
  */
 package net.blackburn;
 
 public class Const {
    
     // this is the logo dimention on the loading screen
-    public static int px =  0;
-    public static int py = 0;
+    public static int px =  160;
+    public static int py = 50;
     public static int  pTextureWidth = 120;
     public static int pTextureHeight= 120;
 
