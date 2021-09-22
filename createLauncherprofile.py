78e8bcf5f4238cf51c7325641808fb892460778c
NicholasBlackburn1 <nickblackburn02@gmail.com>
2021-09-22 18:04:29 -0400

    added working python generation script

--- createLauncherprofile.py
+++ createLauncherprofile.py
@@ -3,11 +3,13 @@ this is for creating  and ziping the gernerated output of minecrft
 """
 
 import json 
-
+import os 
 import pathlib
 import re
 
 
+
+#* gets the project file  version
 def getGradleProgjectName():
 
   with open('settings.gradle','r') as data :
@@ -18,18 +20,16 @@ def getGradleProgjectName():
             line[0] = line[0].strip()
 
             if line[0] == 'rootProject.name':
-                project = re.sub(r'[]\[\' ]','', line[1].replace('"','')).split(',')
+                project = re.sub(r'[]\[\' ]','', line[1].replace('"',str())).split(',')
                 print("got project name"+ " "+str(project[0]))
 
-                return project[0]
+                return str(project[0])
 
 
-
+#* gets the gradle projct version 
 def getGradleProjectVersion():
     version = 'version ='
-    flag = 0
-    index = 0
-        
+
     with open('build.gradle','r') as data :
        
         for line in data:
@@ -38,39 +38,43 @@ def getGradleProjectVersion():
             line[0] = line[0].strip()
 
             if line[0] == 'version':
-                project = re.sub(r'[]\[\' ]','', line[1].strip()).split(',')
+                project = re.sub(r'[]\[\']','', line[1].rstrip().lstrip()).split(',')
                 print("got project verion"+ " "+str(project[0]))
 
-                return project[0]
+                return str(project[0])
 
-
+#* reads the base json to create launcher file 
 def readBaseJson():
-
+    name = getGradleProgjectName().strip()+" "+getGradleProjectVersion()
     with open('base.json') as json_file:
         print("getting ready to write genetated")
         print("opening json file")
         data = json.load(json_file) 
-        data['id'] =  getGradleProgjectName()+" "+getGradleProjectVersion()
+        data['id'] =  name
 
-        print( data['id'])
+        print( "UwU id "+data['id'])
         
-        if data['id'] ==  getGradleProgjectName()+" "+getGradleProjectVersion():
-            with open('test.json', 'w') as outfile:
-                json.dump(data, outfile,indent=4)
+        print("file name"+"profile/"+name+'.json')
+        with open("profile/"+name+'.json', 'w') as outfile:
+            json.dump(data, outfile,indent=4)
 
             
 
 
 #* this is for finding, dumping and creating new launcher json file for new released version
 def createlauncherjson():
-    pass
+    readBaseJson()
+   
+    
+   
 
 
 
 
 def main():
-   readBaseJson()
-
+   
+    createlauncherjson()
+    
 
 
 
