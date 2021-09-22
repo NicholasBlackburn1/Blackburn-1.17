c902f662d77967e6e086518290c1b671466eeb7c
NicholasBlackburn1 <nickblackburn02@gmail.com>
2021-09-22 16:22:16 -0400

    added generated json file

--- createLauncherprofile.py
+++ createLauncherprofile.py
@@ -8,13 +8,30 @@ import pathlib
 import re
 
 
+def getGradleProgjectName():
+
+  with open('settings.gradle','r') as data :
+       
+        for line in data:
+
+            line = line.split('=')
+            line[0] = line[0].strip()
+
+            if line[0] == 'rootProject.name':
+                project = re.sub(r'[]\[\' ]','', line[1].replace('"','')).split(',')
+                print("got project name"+ " "+str(project[0]))
+
+                return project[0]
+
+
+
 def getGradleProjectVersion():
     version = 'version ='
     flag = 0
     index = 0
         
     with open('build.gradle','r') as data :
-        print("opening gradle file")
+       
         for line in data:
 
             line = line.split('=')
@@ -30,8 +47,18 @@ def getGradleProjectVersion():
 def readBaseJson():
 
     with open('base.json') as json_file:
-        data = json.load(json_file)
-        return data
+        print("getting ready to write genetated")
+        print("opening json file")
+        data = json.load(json_file) 
+        data['id'] =  getGradleProgjectName()+" "+getGradleProjectVersion()
+
+        print( data['id'])
+        
+        if data['id'] ==  getGradleProgjectName()+" "+getGradleProjectVersion():
+            with open('test.json', 'w') as outfile:
+                json.dump(data, outfile,indent=4)
+
+            
 
 
 #* this is for finding, dumping and creating new launcher json file for new released version
@@ -42,7 +69,7 @@ def createlauncherjson():
 
 
 def main():
-   getGradleProjectVersion()
+   readBaseJson()
 
 
 
