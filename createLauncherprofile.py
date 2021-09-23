bf0962f5c890e438dd99e886aea6c199c0fc3549
NicholasBlackburn1 <nickblackburn02@gmail.com>
2021-09-23 09:14:39 -0400

    added working pythin script

--- createLauncherprofile.py
+++ createLauncherprofile.py
@@ -6,6 +6,7 @@ import json
 import os 
 import pathlib
 import re
+import shutil
 
 
 
@@ -43,9 +44,19 @@ def getGradleProjectVersion():
 
                 return str(project[0])
 
+profile =getGradleProgjectName().strip()+str("-")+getGradleProjectVersion()+"/"
+
+def copyfile():
+    
+    shutil.copyfile("build/libs/"+str(getGradleProgjectName().strip()+"-"+getGradleProjectVersion()+".jar"), profile+str(getGradleProgjectName().strip()+"-"+getGradleProjectVersion()+".jar"))
+   
+
 #* reads the base json to create launcher file 
 def readBaseJson():
-    name = getGradleProgjectName().strip()+" "+getGradleProjectVersion()
+   
+  
+    name = getGradleProgjectName().strip()+str("-")+getGradleProjectVersion()
+
     with open('base.json') as json_file:
         print("getting ready to write genetated")
         print("opening json file")
@@ -54,19 +65,31 @@ def readBaseJson():
 
         print( "UwU id "+data['id'])
         
-        print("file name"+"profile/"+name+'.json')
-        with open("profile/"+name+'.json', 'w') as outfile:
+        print("file name"+profile+name+'.json')
+        with open(profile+name+'.json', 'w') as outfile:
             json.dump(data, outfile,indent=4)
 
-            
+
+def zip_file(zipname,root):
+    shutil.make_archive(
+       str(zipname), 
+        'zip',           # the archive format - or tar, bztar, gztar 
+        root_dir=root)   # sta
 
 
 #* this is for finding, dumping and creating new launcher json file for new released version
 def createlauncherjson():
-    readBaseJson()
-   
     
-   
+    os.makedirs(getGradleProgjectName().strip()+str("-")+getGradleProjectVersion()+"/")
+    readBaseJson()
+    copyfile()
+    if( os.path.isdir(getGradleProgjectName().strip()+str("-")+getGradleProjectVersion()+"/")):
+        zip_file(zipname=getGradleProgjectName().strip()+str("-")+getGradleProjectVersion(),root="./"+getGradleProgjectName().strip()+str("-")+getGradleProjectVersion()+"/")
+        os.remove(getGradleProgjectName().strip()+str("-")+getGradleProjectVersion()+"/")
+    else:
+        Exception("CANNOT MAKE FILE")
+        
+    
 
 
 
